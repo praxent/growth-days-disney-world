703a6e0ecfa1bf186c1d5d7df227d91d49b452a7
Kevin King <kevin.king@praxent.com>
2021-11-04 19:36:26 -0400

    Change colors for POC

--- src/animal-kingdom/activity3/Activity3.tsx
+++ src/animal-kingdom/activity3/Activity3.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from "styled-components";
 
 const Styles = styled.div`
-  color: red;
+  color: green;
 `;
 
 function Activity3() {
--- src/blizzard-beach/activity2/Activity2.tsx
+++ src/blizzard-beach/activity2/Activity2.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from "styled-components";
 
 const Styles = styled.div`
-  color: red;
+  color: blue;
 `;
 
 function Activity2() {
