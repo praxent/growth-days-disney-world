bc4c6aca199f715f327f09e72414d3142c076c72
Kevin King <kevin.king@praxent.com>
2021-11-04 16:12:16 -0400

    Move pulse animation to be reusable

--- src/disney-world/DisneyWorld.tsx
+++ src/disney-world/DisneyWorld.tsx
@@ -9,10 +9,10 @@ function DisneyWorld() {
         <div className="map-container">
           <img src={disneyWorld} className="map"/>
           <div className="labels">
-            <Link to={"/magic-kingdom"}><span className="label magic-kingdom">Magic Kingdom</span></Link>
-            <Link to={"/blizzard-beach"}><span className="label blizzard-beach">Blizzard Beach</span></Link>
-            <Link to={"/animal-kingdom"}><span className="label animal-kingdom">Animal Kingdom</span></Link>
-            <Link to={"/epcot"}><span className="label epcot">Epcot</span></Link>
+            <Link to={"/magic-kingdom"}><span className="label pulse magic-kingdom">Magic Kingdom</span></Link>
+            <Link to={"/blizzard-beach"}><span className="label pulse blizzard-beach">Blizzard Beach</span></Link>
+            <Link to={"/animal-kingdom"}><span className="label pulse animal-kingdom">Animal Kingdom</span></Link>
+            <Link to={"/epcot"}><span className="label pulse epcot">Epcot</span></Link>
           </div>
         </div>
       </div>
