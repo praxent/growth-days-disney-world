c6af101d005afb1f5e045f1fe62e30200b4fb7c3
Kevin King <kevin.king@praxent.com>
2021-11-09 17:11:40 -0500

    Added a better template for each activity and added a 3rd task for bonus time

--- captain-only/Instructions.tsx
+++ captain-only/Instructions.tsx
@@ -12,11 +12,11 @@ const Styles = styled.div`
     left: 5px;
   }
   
-  .instructions,
-  .spotlight {
+  .instructions {
     position: fixed;
     top: 0;
     z-index: 9998;
+    max-width: 600px;
   
     padding: 40px 50px 20px 50px;
     background: white;
@@ -25,10 +25,6 @@ const Styles = styled.div`
   
   .instructions {
     left: 0;
-  }
-  
-  .spotlight {
-    right: 0;
   }
   
   .instructions ul {
@@ -74,14 +70,16 @@ function renderHelpDetails(pathname : string) {
       helpDetails = (
         <>
           <h2>Requirements</h2>
+
           <h3>Task 1: Squirt the targets at Fossil Fun Games <span className="eta"><img className="clock" src={clock} /> 15 min</span></h3>
           <ul>
             <li>Get an image of the ride's target (if possible) and display it</li>
-            <li>use React's useState hook to to set a variable called shooting to true or false</li>
+            <li>Use React's useState hook to to set a variable called shooting to true or false</li>
             <li>When shooting is true, there should be some visual indication, like water hitting the target from a gun.</li>
           </ul>
           <ul>
           </ul>
+
           <h3>Task 2: Make the squirt gun a child component<span className="eta"><img className="clock" src={clock} /> 30 min</span></h3>
           <ul>
             <li>Refactor the squirt gun to be a child component of the Game component</li>
@@ -89,13 +87,16 @@ function renderHelpDetails(pathname : string) {
             <li>Ensure the functionality still works as it did before</li>
           </ul>
 
+          <h3>Task 3: Bonus!</h3>
+          <ul>
+            <li>If you finish early, add an additional creative feature or functionality. This will help make your case for the cash prize at the end!</li>
+          </ul>
+
           <h2>Feature Spotlight</h2>
           <ul>
             <li><a href="https://reactjs.org/docs/hooks-state.html" target="_blank">useState</a></li>
             <li><a href="https://reactjs.org/docs/components-and-props.html" target="_blank">React props</a></li>
             <li><a href="https://reactjs.org/docs/lifting-state-up.html" target="_blank">Inter-Component Communication</a></li>
-            <li>Effect Hooks</li>
-            <li><a href="https://reactjs.org/docs/higher-order-components.html" target="_blank">Higher Order Component</a></li>
           </ul>
         </>
       );
@@ -104,12 +105,23 @@ function renderHelpDetails(pathname : string) {
       helpDetails = (
         <>
           <h2>Requirements</h2>
-          <ul>
-            <li>AK2</li>
-          </ul>
-          <h2>Feature Spotlight</h2>
+          <h3>Task 1: TODO <span className="eta"><img className="clock" src={clock} /> 15 min</span></h3>
           <ul>
             <li>TODO</li>
+          </ul>
+          <h3>Task 1: TODO <span className="eta"><img className="clock" src={clock} /> 30 min</span></h3>
+          <ul>
+            <li>TODO</li>
+          </ul>
+
+          <h3>Task 3: Bonus!</h3>
+          <ul>
+            <li>If you finish early, add an additional creative feature or functionality. This will help make your case for the cash prize at the end!</li>
+          </ul>
+
+          <h2>Feature Spotlight</h2>
+          <ul>
+            <li><a href="TODO" target="_blank">TODO</a></li>
           </ul>
         </>
       );
@@ -118,12 +130,23 @@ function renderHelpDetails(pathname : string) {
       helpDetails = (
         <>
           <h2>Requirements</h2>
-          <ul>
-            <li>AK3</li>
-          </ul>
-          <h2>Feature Spotlight</h2>
+          <h3>Task 1: TODO <span className="eta"><img className="clock" src={clock} /> 15 min</span></h3>
           <ul>
             <li>TODO</li>
+          </ul>
+          <h3>Task 1: TODO <span className="eta"><img className="clock" src={clock} /> 30 min</span></h3>
+          <ul>
+            <li>TODO</li>
+          </ul>
+
+          <h3>Task 3: Bonus!</h3>
+          <ul>
+            <li>If you finish early, add an additional creative feature or functionality. This will help make your case for the cash prize at the end!</li>
+          </ul>
+
+          <h2>Feature Spotlight</h2>
+          <ul>
+            <li><a href="TODO" target="_blank">TODO</a></li>
           </ul>
         </>
       );
@@ -132,17 +155,24 @@ function renderHelpDetails(pathname : string) {
       helpDetails = (
         <>
           <h2>Requirements</h2>
+
           <h3>Task 1: Ride Summit Plummet at Blizzard Beach <span className="eta"><img className="clock" src={clock} /> 15 min</span></h3>
           <ul>
             <li>Get an image of the slide from Google and display it on the page</li>
-            <li>use React's useState hook to to set a variable called riding to true or false</li>
+            <li>Use React's useState hook to to set a variable called riding to true or false</li>
             <li>When riding is true, there should be some visual indication, like a person moving down the slide.</li>
           </ul>
+
           <h3>Task 2: Make the slide a child component<span className="eta"><img className="clock" src={clock} /> 30 min</span></h3>
           <ul>
             <li>Refactor the slide to be a child component of the Park component</li>
             <li>Pass the two variables from the use state hooks (value and setter) to the child component as props</li>
             <li>Ensure the functionality still works as it did before</li>
+          </ul>
+
+          <h3>Task 3: Bonus!</h3>
+          <ul>
+            <li>If you finish early, add an additional creative feature or functionality. This will help make your case for the cash prize at the end!</li>
           </ul>
           
           <h2>Feature Spotlight</h2>
@@ -158,12 +188,23 @@ function renderHelpDetails(pathname : string) {
       helpDetails = (
         <>
           <h2>Requirements</h2>
-          <ul>
-            <li>BB2</li>
-          </ul>
-          <h2>Feature Spotlight</h2>
+          <h3>Task 1: TODO <span className="eta"><img className="clock" src={clock} /> 15 min</span></h3>
           <ul>
             <li>TODO</li>
+          </ul>
+          <h3>Task 1: TODO <span className="eta"><img className="clock" src={clock} /> 30 min</span></h3>
+          <ul>
+            <li>TODO</li>
+          </ul>
+
+          <h3>Task 3: Bonus!</h3>
+          <ul>
+            <li>If you finish early, add an additional creative feature or functionality. This will help make your case for the cash prize at the end!</li>
+          </ul>
+
+          <h2>Feature Spotlight</h2>
+          <ul>
+            <li><a href="TODO" target="_blank">TODO</a></li>
           </ul>
         </>
       );
@@ -172,12 +213,23 @@ function renderHelpDetails(pathname : string) {
       helpDetails = (
         <>
           <h2>Requirements</h2>
-          <ul>
-            <li>BB3</li>
-          </ul>
-          <h2>Feature Spotlight</h2>
+          <h3>Task 1: TODO <span className="eta"><img className="clock" src={clock} /> 15 min</span></h3>
           <ul>
             <li>TODO</li>
+          </ul>
+          <h3>Task 1: TODO <span className="eta"><img className="clock" src={clock} /> 30 min</span></h3>
+          <ul>
+            <li>TODO</li>
+          </ul>
+
+          <h3>Task 3: Bonus!</h3>
+          <ul>
+            <li>If you finish early, add an additional creative feature or functionality. This will help make your case for the cash prize at the end!</li>
+          </ul>
+
+          <h2>Feature Spotlight</h2>
+          <ul>
+            <li><a href="TODO" target="_blank">TODO</a></li>
           </ul>
         </>
       );
@@ -186,18 +238,26 @@ function renderHelpDetails(pathname : string) {
       helpDetails = (
         <>
           <h2>Requirements</h2>
+
           <h3>Task 1: Ride Spaceship Earth at Epcot <span className="eta"><img className="clock" src={clock} /> 15 min</span></h3>
           <ul>
             <li>Get an image of the ride and display it on the page</li>
-            <li>use React's useState hook to to set a variable called riding to true or false</li>
+            <li>Use React's useState hook to to set a variable called riding to true or false</li>
             <li>When riding is true, there should be some visual indication, like the globe rotating.</li>
           </ul>
+
           <h3>Task 2: Make the globe a child component<span className="eta"><img className="clock" src={clock} /> 30 min</span></h3>
           <ul>
             <li>Refactor the globe to be a child component of the Park component</li>
             <li>Pass the two variables from the use state hooks (value and setter) to the child component as props</li>
             <li>Ensure the functionality still works as it did before</li>
           </ul>
+
+          <h3>Task 3: Bonus!</h3>
+          <ul>
+            <li>If you finish early, add an additional creative feature or functionality. This will help make your case for the cash prize at the end!</li>
+          </ul>
+
           <h2>Feature Spotlight</h2>
           <ul>
             <li><a href="https://reactjs.org/docs/hooks-state.html" target="_blank">useState</a></li>
@@ -211,12 +271,23 @@ function renderHelpDetails(pathname : string) {
       helpDetails = (
         <>
           <h2>Requirements</h2>
-          <ul>
-            <li>E2</li>
-          </ul>
-          <h2>Feature Spotlight</h2>
+          <h3>Task 1: TODO <span className="eta"><img className="clock" src={clock} /> 15 min</span></h3>
           <ul>
             <li>TODO</li>
+          </ul>
+          <h3>Task 1: TODO <span className="eta"><img className="clock" src={clock} /> 30 min</span></h3>
+          <ul>
+            <li>TODO</li>
+          </ul>
+
+          <h3>Task 3: Bonus!</h3>
+          <ul>
+            <li>If you finish early, add an additional creative feature or functionality. This will help make your case for the cash prize at the end!</li>
+          </ul>
+
+          <h2>Feature Spotlight</h2>
+          <ul>
+            <li><a href="TODO" target="_blank">TODO</a></li>
           </ul>
         </>
       );
@@ -225,12 +296,23 @@ function renderHelpDetails(pathname : string) {
       helpDetails = (
         <>
           <h2>Requirements</h2>
-          <ul>
-            <li>E3</li>
-          </ul>
-          <h2>Feature Spotlight</h2>
+          <h3>Task 1: TODO <span className="eta"><img className="clock" src={clock} /> 15 min</span></h3>
           <ul>
             <li>TODO</li>
+          </ul>
+          <h3>Task 1: TODO <span className="eta"><img className="clock" src={clock} /> 30 min</span></h3>
+          <ul>
+            <li>TODO</li>
+          </ul>
+
+          <h3>Task 3: Bonus!</h3>
+          <ul>
+            <li>If you finish early, add an additional creative feature or functionality. This will help make your case for the cash prize at the end!</li>
+          </ul>
+
+          <h2>Feature Spotlight</h2>
+          <ul>
+            <li><a href="TODO" target="_blank">TODO</a></li>
           </ul>
         </>
       );
@@ -239,18 +321,26 @@ function renderHelpDetails(pathname : string) {
       helpDetails = (
         <>
           <h2>Requirements</h2>
+
           <h3>Task 1: Ride the Pirates of the Caribbean boat at Adventureland <span className="eta"><img className="clock" src={clock} /> 15 min</span></h3>
           <ul>
             <li>Get an image of the ride's boat from Google and display it on the page</li>
             <li>use React's useState hook to to set a variable called riding to true or false</li>
             <li>When riding is true, there should be some visual indication, like moving the boat.</li>
           </ul>
+
           <h3>Task 2: Make the ride a child component<span className="eta"><img className="clock" src={clock} /> 30 min</span></h3>
           <ul>
             <li>Refactor the ride to be a child component of the Park component</li>
             <li>Pass the two variables from the use state hooks (value and setter) to the child component as props</li>
             <li>Ensure the functionality still works as it did before</li>
           </ul>
+
+          <h3>Task 3: Bonus!</h3>
+          <ul>
+            <li>If you finish early, add an additional creative feature or functionality. This will help make your case for the cash prize at the end!</li>
+          </ul>
+
           <h2>Feature Spotlight</h2>
           <ul>
             <li><a href="https://reactjs.org/docs/hooks-state.html" target="_blank">useState</a></li>
@@ -264,12 +354,23 @@ function renderHelpDetails(pathname : string) {
       helpDetails = (
         <>
           <h2>Requirements</h2>
-          <ul>
-            <li>MK2</li>
-          </ul>
-          <h2>Feature Spotlight</h2>
+          <h3>Task 1: TODO <span className="eta"><img className="clock" src={clock} /> 15 min</span></h3>
           <ul>
             <li>TODO</li>
+          </ul>
+          <h3>Task 1: TODO <span className="eta"><img className="clock" src={clock} /> 30 min</span></h3>
+          <ul>
+            <li>TODO</li>
+          </ul>
+
+          <h3>Task 3: Bonus!</h3>
+          <ul>
+            <li>If you finish early, add an additional creative feature or functionality. This will help make your case for the cash prize at the end!</li>
+          </ul>
+
+          <h2>Feature Spotlight</h2>
+          <ul>
+            <li><a href="TODO" target="_blank">TODO</a></li>
           </ul>
         </>
       );
@@ -278,12 +379,23 @@ function renderHelpDetails(pathname : string) {
       helpDetails = (
         <>
           <h2>Requirements</h2>
-          <ul>
-            <li>MK3</li>
-          </ul>
-          <h2>Feature Spotlight</h2>
+          <h3>Task 1: TODO <span className="eta"><img className="clock" src={clock} /> 15 min</span></h3>
           <ul>
             <li>TODO</li>
+          </ul>
+          <h3>Task 1: TODO <span className="eta"><img className="clock" src={clock} /> 30 min</span></h3>
+          <ul>
+            <li>TODO</li>
+          </ul>
+
+          <h3>Task 3: Bonus!</h3>
+          <ul>
+            <li>If you finish early, add an additional creative feature or functionality. This will help make your case for the cash prize at the end!</li>
+          </ul>
+
+          <h2>Feature Spotlight</h2>
+          <ul>
+            <li><a href="TODO" target="_blank">TODO</a></li>
           </ul>
         </>
       );
