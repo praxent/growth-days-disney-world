253c6da01bd96a4caa1a0fd6a167214ee7c9d43c
Andres Felipe Ospina Lenis <andresospina0000@gmail.com>
2023-04-14 12:34:14 -0500

    Activities 1 and 2.

--- src/blizzard-beach/activity2/Activity2.tsx
+++ src/blizzard-beach/activity2/Activity2.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from "styled-components";
 
 const Styles = styled.div`
@@ -8,16 +8,44 @@ const Styles = styled.div`
     max-width: 900px;
     margin: 0 auto;
   }
+
 `;
 
+const endpoint = 'http://localhost:4000/lottawatta_lodge';
+
 function Activity2() {
+
+
+  try {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+
+      fetch(endpoint)
+        .then((res) => res.json())
+        .then((result) => {
+          setPosts(result);
+          console.log(result);
+        });
+    }, []);
+  } catch (error) {
+    console.log(error);
+  }
+
+
   return (
     <Styles>
       <div className="container">
-        Hello from Activity 2 Blizzard Beach
+        <img id="lottaLodge" src="/src/blizzard-beach/img/lottawatta_lodge.webp" alt="" />
+      </div>
+      <div className='row'>
+
       </div>
     </Styles>
   )
 }
 
+
 export default Activity2
+
