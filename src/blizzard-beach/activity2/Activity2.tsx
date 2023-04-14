b902137e42133b18317e676ad025e32b7b70a104
marcusvseixas <mvssk8@yahoo.com.br>
2023-04-14 12:18:09 -0300

    initial activities

--- src/blizzard-beach/activity2/Activity2.tsx
+++ src/blizzard-beach/activity2/Activity2.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import styled from "styled-components";
 
 const Styles = styled.div`
@@ -11,10 +11,71 @@ const Styles = styled.div`
 `;
 
 function Activity2() {
+  const [menuArray, setData] = useState([]);
+  const initialValue = { id: 0, name: "", description:"", type:"", quantity:0 };
+  const [menu, setMenu] = useState([initialValue]);
+  const image = 'https://www.disneyfoodblog.com/wp-content/uploads/2021/03/2021-wdw-blizzard-beach-reopening-lottawatta-lodge-2-1.jpg'
+
+
+  function buyAction(item){    
+    let updatedMenu = menuArray.map((menuItem) => {
+      if (menuItem.id === item.id) {
+          return { ...menuItem, quantity: item.quantity - 1 }
+      }
+      
+      return menuItem
+  })
+  
+  setData(updatedMenu)
+
+  }
+
+  useEffect(() => {
+    fetch('http://localhost:4000/lottawatta_lodge')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setData(data);
+  })},[])
+
   return (
     <Styles>
-      <div className="container">
-        Hello from Activity 2 Blizzard Beach
+      <div>
+        <div>
+          <img/>
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Product</th>
+                <th>Description</th>
+                <th>For</th>
+                <th>Quantity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {menuArray.map(item => {
+                return (
+                  <tr key={item.id}>
+                    {(item.type === 'Adult' || item.type === 'Child') ? (
+                    <td>{item.quantity > 0 ? <button className="buttonMenu" onClick={() => buyAction(item)}>Buy</button> : ''}</td>
+                     ) : (
+                    <td>{item.quantity > 0 && item.type === 'Child' ? <button className="buttonMenu" onClick={() => buyAction(item)}>Buy</button> : ''}</td>
+                     )}
+                    <td>{ item.name }</td>
+                    <td className='descriptionColumn'>{ item.description }</td>
+                    <td>{ item.type }</td>
+                    <td>{ item.quantity }</td>
+                    
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+
+          
+          </div>
+
       </div>
     </Styles>
   )
--- src/blizzard-beach/activity3/Activity3.tsx
+++ src/blizzard-beach/activity3/Activity3.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import styled from "styled-components";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import CrossCountryCreekImg from "./images/Cross-Country-Creek.jpg";
+import MeltAwayBayImg from "./images/Melt-Away-Bay.jpg";
+import RunoffRapidsImg from "./images/Runoff-Rapids.jpg";
+import SnowStormersImg from "./images/Snow-Stormers.jpg";
+import Waves from "./sounds/waves.mp3";
+import CannonBall from "./sounds/cannonball.mp3";
+import Draining from "./sounds/draining.mp3";
+import Crickets from "./sounds/crickets.mp3";
 
 const Styles = styled.div`
   color: green;
@@ -10,12 +19,105 @@ const Styles = styled.div`
   }
 `;
 
+function CrossCountryCreek() {
+ 
+  return (
+      <React.Fragment>
+        <img src={CrossCountryCreekImg} className="imgcontainer"/>;
+      </React.Fragment> 
+  )
+  
+}
+
+let audio: HTMLAudioElement | null = null; // Variável para armazenar a instância do objeto Audio
+
+function playSound(sound: any) {
+  if (audio) {
+    audio.pause(); // Pausa o áudio anterior, se houver
+  }
+  
+  audio = new Audio(sound); // Cria uma nova instância do objeto Audio
+  audio.play();
+}
+
+function MeltAwayBay() {
+  return (
+    <React.Fragment>
+      <img src={MeltAwayBayImg} className="imgcontainer"/>;
+    </React.Fragment> 
+)
+}
+
+
+function RunoffRapids() {
+  return (
+    <React.Fragment>
+      <img src={RunoffRapidsImg} className="imgcontainer"/>;
+    </React.Fragment> 
+)
+}
+
+function SnowStormers() {
+  return (
+    <React.Fragment>
+      <img src={SnowStormersImg} className="imgcontainer"/>;
+    </React.Fragment> 
+)
+}
+
 function Activity3() {
+  const initialValue = { id: 0, name: "", description:"", type:"", quantity:0 };
+  const [menu, setMenu] = useState([initialValue]);
+
   return (
     <Styles>
-      <div className="container">
-        Hello from Activity 3 Blizzard Beach
-      </div>
+
+      <h1>Cameras</h1>
+      <div className=".container">
+        <div className="map-container3">
+          <div className='crossCountryCreek'>
+            <Link to="crossCountryCreek" className=''> 
+            <button type="button" className='btn ' onClick={() => playSound(Waves)} >
+              Cross Country Creek
+            </button></Link>{" "}
+          </div>
+          <div className='meltAwayBay'>
+            <Link to="meltAwayBay"> 
+            <button type="button" className='btn' onClick={() => playSound(Crickets)} >
+              Melt Away Bay
+            </button></Link>{" "}
+          </div>
+          <div className='runoffRapids'>
+            <Link to="runoffRapids"> 
+            <button type="button" className='btn'onClick={() => playSound(Draining)} >
+              Runoff Rapids
+            </button></Link>{" "}
+          </div>
+          <div className='snowStormers'>
+            <Link to="snowStormers"> 
+            <button type="button" className='btn'onClick={() => playSound(CannonBall)} >
+              Snow Stormers
+            </button></Link>{" "}
+          </div>
+          <div className=''>
+          </div>
+          
+              <Outlet />
+          </div>
+        </div>
+      
+
+
+     <Routes>
+     
+        <Route path="crossCountryCreek" element={<CrossCountryCreek />} />
+        <Route path="meltAwayBay" element={<MeltAwayBay />} />
+        <Route path="runoffRapids" element={<RunoffRapids />} />
+        <Route path="snowStormers" element={<SnowStormers />} />
+
+      
+    </Routes>
+
     </Styles>
   )
 }
